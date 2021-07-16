ab488f3faa4f998ac91243efccb6df2399ba55cc
mohammad012 <mooode2261@gmail.com>
2021-07-17 00:28:39 +0300

    add job views& url

--- job/models.py
+++ job/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
+
+JOB_TYPE=(
+        ('full time','Full Time'),
+        ('part time','Part Time'),
+        )
 # Create your models here.
+class job(models.Model): #table
+    title = models.CharField(max_length=100) #column
+    job_type =models.CharField(max_length=50, choices=JOB_TYPE)
+    description =models.TextField(max_length=1000)
+    published_at =models.DateTimeField(auto_now=True)
+    vacancy =models.IntegerField(default=1)
+    salary =models.IntegerField(default=0)
+    experience =models.IntegerField(default=1)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+
+
+    def __str__(self) :
+        return self.title
+
+class Category(models.Model):
+    name =models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
